1ffdb122398aac8d74a48111e5289860c6b6faa9
francoleon42 <francoleonadress@gmail.com>
2024-09-30 18:26:44 -0300

    mantenimiento  creacion de flujo

---
+++ src/main/java/com/gifa_api/service/IMantenimientoService.java
@@ -0,0 +1,6 @@
+package com.gifa_api.service;
+
+public interface IMantenimientoService {
+    void registrarMantenimientoManualmente(Integer id);
+    void FinalizarMantenimiento(Integer id);
+}
---
+++ src/main/java/com/gifa_api/service/impl/MantenimientoService.java
@@ -0,0 +1,17 @@
+package com.gifa_api.service.impl;
+
+import com.gifa_api.service.IMantenimientoService;
+
+public class MantenimientoService implements IMantenimientoService {
+
+
+    @Override
+    public void registrarMantenimientoManualmente(Integer id) {
+
+    }
+
+    @Override
+    public void FinalizarMantenimiento(Integer id) {
+
+    }
+}
