110da477a41431d19346db50b840739d2186ab7c
francoleon42 <francoleonadress@gmail.com>
2024-10-05 10:14:46 -0300

    hacer pedido

--- src/main/java/com/gifa_api/service/impl/ItemDeInventarioServiceImpl.java
+++ src/main/java/com/gifa_api/service/impl/ItemDeInventarioServiceImpl.java
@@ -1,13 +1,20 @@
 package com.gifa_api.service.impl;
 
 import com.gifa_api.dto.RegistrarItemDeInventarioDTO;
+import com.gifa_api.dto.mantenimiento.RegistrarMantenimientoDTO;
 import com.gifa_api.exception.NotFoundException;
 import com.gifa_api.model.ItemDeInventario;
+import com.gifa_api.model.Vehiculo;
 import com.gifa_api.repository.ItemDeInventarioRepository;
 import com.gifa_api.service.IItemDeIventarioService;
 import com.gifa_api.service.IMantenimientoService;
+import com.gifa_api.service.IPedidoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -35,8 +42,9 @@ public class ItemDeInventarioServiceImpl implements IItemDeIventarioService {
         if(itemIventario.getUmbral() <  itemIventario.getStock() - 1 ){
             itemIventario.desminuirStock();
         }
-
         itemDeInventarioRepository.save(itemIventario);
 
     }
+
+
 }
