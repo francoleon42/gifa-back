eaefd46daad99a23fbcb4cf3ad23dd3ba4f8e919
francoleon42 <francoleonadress@gmail.com>
2024-10-05 16:40:19 -0300

    create CorsWebConfig

---
+++ src/main/java/com/gifa_api/config/CorsWebConfig.java
@@ -0,0 +1,18 @@
+package com.gifa_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")  // Permitir solo desde React (puerto 3000)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
