a9cbce30e801d0df01ebb080d8d9a5af479e9c3e
Tomas Ledesma <tomasld13@gmail.com>
2024-10-05 14:41:09 -0300

    fix: arreglo en rutas

--- src/main/java/com/gifa_api/controller/AuthController.java
+++ src/main/java/com/gifa_api/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.gifa_api.dto.login.LoginRequestDTO;
 import com.gifa_api.dto.login.RegisterRequestDTO;
 import com.gifa_api.service.IAuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,6 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterRequestDTO registerRequestDto){
-        return ResponseEntity.ok(authService.register(registerRequestDto));
+        return new ResponseEntity<>(authService.register(registerRequestDto), HttpStatus.CREATED);
     }
 }
