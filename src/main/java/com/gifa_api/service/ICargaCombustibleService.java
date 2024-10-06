878ad35f5b466cef29b4c0362d5c3fe29aba1508
francoleon42 <francoleonadress@gmail.com>
2024-10-06 19:40:41 -0300

    add cargar Combustible

---
+++ src/main/java/com/gifa_api/service/ICargaCombustibleService.java
@@ -0,0 +1,8 @@
+package com.gifa_api.service;
+
+import com.gifa_api.dto.gestionDeCombustilble.CargaCombustibleRequestDTO;
+
+public interface ICargaCombustibleService {
+
+    void cargarCombustible(CargaCombustibleRequestDTO cargaCombustibleRequestDTO);
+}
