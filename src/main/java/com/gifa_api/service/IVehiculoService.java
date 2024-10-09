cae74e07ea4e3cb4b59f69c3a90f309082c83f5d
Tomas Ledesma <tomasld13@gmail.com>
2024-10-09 20:28:13 -0300

    fix: unused imports

--- src/main/java/com/gifa_api/service/IVehiculoService.java
+++ src/main/java/com/gifa_api/service/IVehiculoService.java
@@ -1,14 +1,9 @@
 package com.gifa_api.service;
 
-import com.gifa_api.dto.vehiculo.AsignarParteRequestDTO;
 import com.gifa_api.dto.vehiculo.ListaVehiculosResponseDTO;
 import com.gifa_api.dto.vehiculo.RegistarVehiculoDTO;
-import com.gifa_api.model.Vehiculo;
-
-import java.util.List;
 
 public interface IVehiculoService {
-
     ListaVehiculosResponseDTO getVehiculos();
     void registrar(RegistarVehiculoDTO registarVehiculoDTO);
     void inhabilitar(Integer idVehiculoToInhabilitar);
