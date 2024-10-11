548a36013fb1691f363f98a2c7a336ecd347999d
francoleon42 <francoleonadress@gmail.com>
2024-10-11 18:14:59 -0300

    Update Chofer.java

--- src/main/java/com/gifa_api/model/Chofer.java
+++ src/main/java/com/gifa_api/model/Chofer.java
@@ -1,4 +1,22 @@
 package com.gifa_api.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chofer")
 public class Chofer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "patente", nullable = false, length = 10)
+    private String nombre;
 }
