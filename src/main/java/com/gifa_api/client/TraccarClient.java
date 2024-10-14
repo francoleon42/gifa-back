9fba4ed64a4875098a4dd816b93dda140c3c2e50
francoleon42 <francoleonadress@gmail.com>
2024-10-14 11:21:59 -0300

    add dependencia de webClient

--- src/main/java/com/gifa_api/client/TraccarClient.java
+++ src/main/java/com/gifa_api/client/TraccarClient.java
@@ -1,4 +1,15 @@
 package com.gifa_api.client;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
 public class TraccarClient {
+
+    private final WebClient webClient;
+
+
+
 }
