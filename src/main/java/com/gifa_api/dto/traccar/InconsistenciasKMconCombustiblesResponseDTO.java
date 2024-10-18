d1b94fa133c999fcc3d4e994d4069e9af15be98e
francoleon42 <francoleonadress@gmail.com>
2024-10-17 22:03:01 -0300

    Update InconsistenciasKMconCombustiblesResponseDTO.java

--- src/main/java/com/gifa_api/dto/traccar/InconsistenciasKMconCombustiblesResponseDTO.java
+++ src/main/java/com/gifa_api/dto/traccar/InconsistenciasKMconCombustiblesResponseDTO.java
@@ -1,4 +1,13 @@
 package com.gifa_api.dto.traccar;
 
+import com.gifa_api.dto.vehiculo.VehiculoResponseDTO;
+
 public class InconsistenciasKMconCombustiblesResponseDTO {
+
+    String nombreChofer;
+    VehiculoResponseDTO vehiculo;
+    Integer kilometrajeRecorrido;
+    Integer litrosCargados;
+    Double litrosInconsistente;
+
 }
