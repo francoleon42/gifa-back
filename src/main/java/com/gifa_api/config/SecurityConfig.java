d4c145b95702a18293e429055d9cdb70110a30a9
francoleon42 <francoleonadress@gmail.com>
2024-10-25 10:45:41 -0300

    add getPosiciones de dispositivo

--- src/main/java/com/gifa_api/config/SecurityConfig.java
+++ src/main/java/com/gifa_api/config/SecurityConfig.java
@@ -158,7 +158,8 @@ public class SecurityConfig {
         authRequest
                 .requestMatchers(HttpMethod.POST, "/traccar/crearDispositivo").hasRole(ADMINISTRADOR)
                 .requestMatchers(HttpMethod.GET, "/traccar/getDispositivos").hasRole(ADMINISTRADOR)
-                .requestMatchers(HttpMethod.GET, "/traccar/verInconsistenciasDeCombustible").hasRole(ADMINISTRADOR);
+                .requestMatchers(HttpMethod.GET, "/traccar/verInconsistenciasDeCombustible").hasRole(ADMINISTRADOR)
+                .requestMatchers(HttpMethod.GET, "/traccar/getPosiciones/{unicoId}").hasRole(ADMINISTRADOR);
 
 
     }
