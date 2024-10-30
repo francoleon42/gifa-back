560df34f0d68b4de5044e2cb3d249da03f3b497d
francoleon42 <francoleonadress@gmail.com>
2024-10-30 10:56:37 -0300

    Update IProveedorDeItemRepository.java

--- src/main/java/com/gifa_api/repository/IProveedorDeItemRepository.java
+++ src/main/java/com/gifa_api/repository/IProveedorDeItemRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface IProveedorDeItemRepository extends JpaRepository<ProveedorDeItem, Integer> {
-    //esto creo que esta mal
+    
     @Query("SELECT p FROM ProveedorDeItem p WHERE p.itemDeInventario.id = :itemId ORDER BY p.precio ASC")
     ProveedorDeItem findProveedorMasEconomicoByItemId(@Param("itemId") Integer itemId);
 }
