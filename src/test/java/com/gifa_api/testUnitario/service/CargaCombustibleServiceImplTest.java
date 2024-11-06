d060b4fa4e0c3adc4a0079b1366463273ca43332
federicoPardo999 <federicopardo944@gmail.com>
2024-11-06 16:58:37 -0300

    update algunos tests

--- src/test/java/com/gifa_api/testUnitario/service/CargaCombustibleServiceImplTest.java
+++ src/test/java/com/gifa_api/testUnitario/service/CargaCombustibleServiceImplTest.java
@@ -1,106 +1,126 @@
-//package com.gifa_api.testUnitario.service;
-//
-//import com.gifa_api.dto.gestionDeCombustilble.CargaCombustibleRequestDTO;
-//import com.gifa_api.exception.BadRequestException;
-//import com.gifa_api.exception.NotFoundException;
-//import com.gifa_api.model.CargaCombustible;
-//import com.gifa_api.model.Tarjeta;
-//import com.gifa_api.repository.ICargaCombustibleRepository;
-//import com.gifa_api.repository.ITarjetaRepository;
-//import com.gifa_api.service.impl.CargaCombustibleServiceImpl;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import java.time.OffsetDateTime;
-//import java.util.Optional;
-//
-//import static java.util.Optional.empty;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.Mockito.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//class CargaCombustibleServiceImplTest {
-//
-//    @Mock
-//    private ITarjetaRepository tarjetaRepository;
-//
-//    @Mock
-//    private ICargaCombustibleRepository cargaCombustibleRepository;
-//
-//    @InjectMocks
-//    private CargaCombustibleServiceImpl cargaCombustibleService;
-//
-//    private CargaCombustibleRequestDTO cargaCombustible;
-//
-//    @BeforeEach
-//    void setUp(){
-//        cargaCombustible  = CargaCombustibleRequestDTO
-//                .builder()
-//                .cantidadLitros(10)
-//                .numeroTarjeta(12345678) // Usando un número entero para la tarjeta
-//                .build();
-//    }
-//
-//    @Test
-//    void cargarCombustible_cantidadDeLitrosNoPuedeSerNull() {
-//        cargaCombustible.setCantidadLitros(null);
-//        verificarNoRegistroDeCargaDeCombustibleInvalida();
-//    }
-//
-//    @Test
-//    void cargarCombustible_cantidadDeLitrosDebeSerPositiva() {
-//        cargaCombustible.setCantidadLitros(0);
-//        verificarNoRegistroDeCargaDeCombustibleInvalida() ;   }
-//
-//    @Test
-//    void cargarCombustible_campoTarjetaNoPuedeSerNull() {
-//        cargaCombustible.setNumeroTarjeta(null);
-//        verificarNoRegistroDeCargaDeCombustibleInvalida();
-//    }
-//
-//    @Test
-//    void cargarCombustible_tarjetaInvalidaLanzaExcepcion() {
-//        when(tarjetaRepository.findById(cargaCombustible.getNumeroTarjeta())).thenReturn(empty());
-//
-//        assertThrows(NotFoundException.class, () -> cargaCombustibleService.cargarCombustible(cargaCombustible));
-//        verify(tarjetaRepository, times(1)).findById(cargaCombustible.getNumeroTarjeta());
-//        verify(cargaCombustibleRepository, never()).save(any(CargaCombustible.class));
-//    }
-//
-//    @Test
-//    void cargarCombustible_tarjetaValida_lanzaExcepcionSiTarjetaNoSeEncuentra() {
-//        Tarjeta tarjeta = new Tarjeta();
-//        tarjeta.setNumero(12345678); // Establecemos un número de tarjeta válido
-//
-//        when(tarjetaRepository.findById(cargaCombustible.getNumeroTarjeta())).thenReturn(Optional.of(tarjeta));
-//        when(cargaCombustibleRepository.save(any(CargaCombustible.class))).thenReturn(new CargaCombustible());
-//
-//        cargaCombustibleService.cargarCombustible(cargaCombustible); // Llamada al método público
-//
-//        verify(cargaCombustibleRepository, times(1)).save(any(CargaCombustible.class));
-//    }
-//
-//    @Test
-//    void cargarCombustible_tarjetaValida_yCargaExitosa() {
-//        Tarjeta tarjeta = new Tarjeta();
-//        tarjeta.setNumero(12345678); // Establecemos un número de tarjeta válido
-//
-//        when(tarjetaRepository.findById(cargaCombustible.getNumeroTarjeta())).thenReturn(Optional.of(tarjeta));
-//        when(cargaCombustibleRepository.save(any(CargaCombustible.class))).thenReturn(new CargaCombustible());
-//
-//        cargaCombustibleService.cargarCombustible(cargaCombustible); // Llamada al método público
-//
-//        verify(cargaCombustibleRepository, times(1)).save(any(CargaCombustible.class));
-//    }
-//
-//    public void verificarNoRegistroDeCargaDeCombustibleInvalida(){
-//        assertThrows(BadRequestException.class, () -> cargaCombustibleService.cargarCombustible(cargaCombustible));
-//        verify(cargaCombustibleRepository,never()).save(any(CargaCombustible.class));
-//    }
-//
-//}
+package com.gifa_api.testUnitario.service;
+
+import com.gifa_api.dto.gestionDeCombustilble.CargaCombustibleRequestDTO;
+import com.gifa_api.exception.BadRequestException;
+import com.gifa_api.exception.NotFoundException;
+import com.gifa_api.model.CargaCombustible;
+import com.gifa_api.model.Tarjeta;
+import com.gifa_api.repository.ICargaCombustibleRepository;
+import com.gifa_api.repository.ITarjetaRepository;
+import com.gifa_api.service.impl.CargaCombustibleServiceImpl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+//corregirr
+@ExtendWith(MockitoExtension.class)
+class CargaCombustibleServiceImplTest {
+
+    @Mock
+    private ITarjetaRepository tarjetaRepository;
+
+    @Mock
+    private ICargaCombustibleRepository cargaCombustibleRepository;
+
+    @InjectMocks
+    private CargaCombustibleServiceImpl cargaCombustibleService;
+
+    private List<CargaCombustible> cargasDeCombustible ;
+    private CargaCombustibleRequestDTO cargaCombustibleDTO;
+
+    @BeforeEach
+    void setUp(){
+        cargaCombustibleDTO = CargaCombustibleRequestDTO
+                .builder()
+                .cantidadLitros(10)
+                .id(1)
+                .build();
+
+        CargaCombustible cargaCombustible = CargaCombustible.builder()
+                .tarjeta(null)
+                .precioPorLitro(1.0f)
+                .costoTotal(3.0f)
+                .fechaHora(LocalDate.now().plusDays(1))
+                .cantidadLitros(10)
+                .build();
+
+        CargaCombustible cargaCombustible2 = CargaCombustible.builder()
+                .tarjeta(null)
+                .precioPorLitro(1.0f)
+                .costoTotal(3.0f)
+                .fechaHora(LocalDate.now().plusDays(1))
+                .cantidadLitros(10)
+                .build();
+
+        cargasDeCombustible = List.of(cargaCombustible,cargaCombustible2);
+
+    }
+
+    @Test
+    void cargarCombustible_cantidadDeLitrosNoPuedeSerNull() {
+        cargaCombustibleDTO.setCantidadLitros(null);
+        verificarNoRegistroDeCargaDeCombustibleInvalida();
+    }
+
+    @Test
+    void cargarCombustible_cantidadDeLitrosDebeSerPositiva() {
+        cargaCombustibleDTO.setCantidadLitros(0);
+        verificarNoRegistroDeCargaDeCombustibleInvalida() ;
+    }
+
+    @Test
+    void cargarCombustibleConCampoTarjetaVacio_lanzaBadRequestException(){
+        cargaCombustibleDTO.setId(null);
+        verificarNoRegistroDeCargaDeCombustibleInvalida() ;
+    }
+
+    @Test
+    void cargarCombustible_tarjetaInvalidaLanzaExcepcion() {
+        when(tarjetaRepository.findById(cargaCombustibleDTO.getId())).thenReturn(empty());
+
+        assertThrows(NotFoundException.class, () -> cargaCombustibleService.cargarCombustible(cargaCombustibleDTO));
+        verify(tarjetaRepository, times(1)).findById(cargaCombustibleDTO.getId());
+        verify(cargaCombustibleRepository, never()).save(any(CargaCombustible.class));
+    }
+
+    @Test
+    void cargarCombustible_tarjetaValida_yCargaExitosa() {
+        Tarjeta tarjeta = new Tarjeta();
+        tarjeta.setNumero(12345678); // Establecemos un número de tarjeta válido
+
+        when(tarjetaRepository.findById(cargaCombustibleDTO.getId())).thenReturn(Optional.of(tarjeta));
+
+        cargaCombustibleService.cargarCombustible(cargaCombustibleDTO); // Llamada al método público
+
+        verify(cargaCombustibleRepository, times(1)).save(any(CargaCombustible.class));
+    }
+
+    @Test
+    void combustibleCargadoEn_devuelveCombustibleCargadoDespuesDeEsaFecha(){
+        when(cargaCombustibleRepository.findByNumeroTarjetaAndFechaAfter(1,LocalDate.now()))
+                .thenReturn(cargasDeCombustible);
+
+        Double totalDeLitros = cargaCombustibleService.combustibleCargadoEn(1,LocalDate.now());
+
+        assertEquals(20,totalDeLitros);
+    }
+
+    public void verificarNoRegistroDeCargaDeCombustibleInvalida(){
+        assertThrows(BadRequestException.class, () -> cargaCombustibleService.cargarCombustible(cargaCombustibleDTO));
+        verify(cargaCombustibleRepository,never()).save(any(CargaCombustible.class));
+    }
+
+}
--- src/test/java/com/gifa_api/testUnitario/service/ChoferServiceImplTest.java
+++ src/test/java/com/gifa_api/testUnitario/service/ChoferServiceImplTest.java
@@ -1,246 +1,214 @@
-//package com.gifa_api.testUnitario.service;
-//
-//import com.gifa_api.dto.chofer.AsignarChoferDTO;
-//import com.gifa_api.dto.chofer.ChoferRegistroDTO;
-//import com.gifa_api.dto.chofer.ChoferResponseDTO;
-//import com.gifa_api.exception.BadRequestException;
-//import com.gifa_api.exception.NotFoundException;
-//import com.gifa_api.model.Chofer;
-//import com.gifa_api.model.Vehiculo;
-//import com.gifa_api.repository.IChoferRepository;
-//import com.gifa_api.repository.IVehiculoRepository;
-//import com.gifa_api.service.impl.ChoferServiceImpl;
-//import com.gifa_api.utils.enums.EstadoChofer;
-//import com.gifa_api.utils.mappers.ChoferMapper;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//import java.util.List;
-//import java.util.Optional;
-//import java.util.Set;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.*;
-//@ExtendWith(MockitoExtension.class)
-//
-//class ChoferServiceImplTest {
-//    @Mock
-//    private IVehiculoRepository vehiculoRepository;
-//
-//    @Mock
-//    private IChoferRepository choferRepository;
-//
-//    @Mock
-//    private  PasswordEncoder passwordEncoder;
-//    @Mock
-//    private  ChoferMapper choferMapper;
-//    @InjectMocks
-//    private ChoferServiceImpl choferService;
-//
-//    private ChoferRegistroDTO choferRegistro;
-//    private Chofer chofer;
-//    private  AsignarChoferDTO asignarChoferDTO;
-//    private Vehiculo vehiculo;
-//    @BeforeEach
-//    void setUp(){
-//        choferRegistro = ChoferRegistroDTO.builder().username("username").password("password").nombre("nombre").build();
-//        asignarChoferDTO =  AsignarChoferDTO.builder().idChofer(1).idVehiculo(1).build();
-//
-//        chofer = Chofer.builder().id(1).nombre("chofer").build();
-//        vehiculo = Vehiculo.builder()
-//                .chofers(Set.of(chofer))
-//                .id(1)
-//                .patente("ABC123")
-//                .build();
-//    }
-//
-//    @Test
-//    void campoUserNameNoPuedeEstarVacio(){
-//        choferRegistro.setUsername("");
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//    @Test
-//    void campoUserNameNoPuedeSerNulo(){
-//        choferRegistro.setUsername(null);
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void campoUserNameNoPuedeContenerCaracteresEspeciales(){
-//        choferRegistro.setUsername("fede#");
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void campoContraseniaoPuedeSerNulo(){
-//        choferRegistro.setPassword(null);
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void campoContraseniaNoPuedeEstarVacio(){
-//        choferRegistro.setPassword("");
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void campoNombreNoPuedeEstarVacio(){
-//        choferRegistro.setNombre("");
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void campoNombreNoPuedeSerNulo(){
-//        choferRegistro.setNombre(null);
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void habilitarChofer_debeLanzarExcepcionSiNoExiste() {
-//        when(choferRepository.findById(1)).thenReturn(Optional.empty());
-//        assertThrows(NotFoundException.class, () -> choferService.habilitar(1));
-//        verify(choferRepository, never()).save(any(Chofer.class));
-//    }
-//
-//
-//    @Test
-//    void inhabilitarChofer_debeLanzarExcepcionSiNoExiste() {
-//        Integer idChofer = 1;
-//        when(choferRepository.findByIdWithVehiculo(idChofer)).thenReturn(Optional.empty());
-//        assertThrows(NotFoundException.class, () -> choferService.inhabilitar(1));
-//        verify(choferRepository, never()).save(any(Chofer.class));
-//    }
-//
-//    @Test
-//
-//    void asignarVehiculo_NoSeEncuentraVehiculoParaElChofer(){
-//        when(vehiculoRepository.findById(asignarChoferDTO.getIdVehiculo())).thenReturn(Optional.empty());
-//
-//        assertThrows(NotFoundException.class,() -> choferService.asignarVehiculo(asignarChoferDTO));
-//
-//        verify(choferRepository,never()).save(any(Chofer.class));
-//        verify(vehiculoRepository,times(1)).findById(asignarChoferDTO.getIdVehiculo());
-//    }
-//
-//    @Test
-//    void asignarVehiculo_NoSeEncuentraElChofer(){
-//        when(vehiculoRepository.findById(asignarChoferDTO.getIdVehiculo())).thenReturn(Optional.of(new Vehiculo()));
-//        when(choferRepository.findById(asignarChoferDTO.getIdChofer())).thenReturn(Optional.empty());
-//
-//        assertThrows(NotFoundException.class,() -> choferService.asignarVehiculo(asignarChoferDTO));
-//
-//        verify(vehiculoRepository,times(1)).findById(asignarChoferDTO.getIdVehiculo());
-//        verify(choferRepository,times(1)).findById(asignarChoferDTO.getIdVehiculo());
-//        verify(choferRepository,never()).save(any(Chofer.class));
-//    }
-//
-//    @Test
-//    void registrarChofer_usuarioVacioLanzaExcepcion(){
-//        choferRegistro.setUsername("");
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void registrarChofer_usuarioConCaracteresEspecialesLanzaExcepcion(){
-//        choferRegistro.setUsername("diegote#");
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//    @Test
-//    void registrarChofer_contraseniaVaciaLanzaExcepcion(){
-//        choferRegistro.setPassword("");
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void registrarChofer_nombreVacioLanzaExcepcion(){
-//        choferRegistro.setNombre("");
-//        verificarNoRegistroDeChoferInvalido();
-//    }
-//
-//    @Test
-//    void asignarVehiculo(){
-//        Vehiculo vehiculo = new Vehiculo();
-//        when(vehiculoRepository.findById(asignarChoferDTO.getIdVehiculo())).thenReturn(Optional.of(vehiculo));
-//        when(choferRepository.findById(asignarChoferDTO.getIdChofer())).thenReturn(Optional.of(chofer));
-//
-//        choferService.asignarVehiculo(asignarChoferDTO);
-//
-//        verify(vehiculoRepository,times(1)).findById(asignarChoferDTO.getIdVehiculo());
-//        verify(choferRepository,times(1)).findById(asignarChoferDTO.getIdChofer());
-//        verify(choferRepository,times(1)).save(any(Chofer.class));
-//        assertEquals(chofer.getVehiculo(),vehiculo);
-//    }
-//
-//    @Test
-//    void registrarChoferDebeGuardarChoferSiVehiculoExiste() {
-//        ChoferRegistroDTO choferRegistroDTO = new ChoferRegistroDTO("chofer","1234","CHOFER");
-//
-//        when(passwordEncoder.encode(choferRegistroDTO.getPassword())).thenReturn("1234");
-//        choferService.registro(choferRegistroDTO);
-//
-//        verify(choferRepository, times(1)).save(any(Chofer.class));
-//    }
-//    @Test
-//    void inhabilitarChofer_debeInhabilitarSiEstaHabilitado() {
-//        chofer.setEstadoChofer(EstadoChofer.HABILITADO);
-//        chofer.setVehiculo(vehiculo);
-//        when(choferRepository.findByIdWithVehiculo(1)).thenReturn(Optional.of(chofer));
-//
-//        choferService.inhabilitar(1);
-//
-//        assertEquals(EstadoChofer.INHABILITADO, chofer.getEstadoChofer());
-//        verify(choferRepository, times(1)).findByIdWithVehiculo(1);
-//        verify(choferRepository, times(1)).save(any(Chofer.class));
-//    }
-//
-//    @Test
-//    void habilitarChofer_debeHabilitarSiEstaInhabilitado() {
-//        chofer.setEstadoChofer(EstadoChofer.INHABILITADO);
-//
-//        when(choferRepository.findById(1)).thenReturn(Optional.of(chofer));
-//
-//        choferService.habilitar(1);
-//
-//        assertEquals(EstadoChofer.HABILITADO, chofer.getEstadoChofer());
-//        verify(choferRepository, times(1)).save(any(Chofer.class));
-//        verify(choferRepository, times(1)).findById(1);
-//    }
-//
-//    @Test
-//    void designarVehiculoDeChoferInhabilitado() {
-//        chofer.setEstadoChofer(EstadoChofer.HABILITADO);
-//        chofer.setVehiculo(vehiculo);
-//        when(choferRepository.findByIdWithVehiculo(1)).thenReturn(Optional.of(chofer));
-//        choferService.inhabilitar(1);
-//
-//        verify(vehiculoRepository,times(1)).save(any(Vehiculo.class));
-//        assertNull(chofer.getVehiculo());
-//    }
-//
-//    @Test
-//    void obtenerTodosLosChoferes(){
-//        ChoferResponseDTO chofer1 = ChoferResponseDTO.builder().nombre("chofer1").build();
-//        ChoferResponseDTO chofer2 = ChoferResponseDTO.builder().nombre("chofer2").build();
-//        List<ChoferResponseDTO> choferes = List.of(chofer1,chofer2);
-//
-//        when(choferMapper.obtenerListaChoferDTO(choferRepository.findAll())).thenReturn(choferes);
-//
-//        List<ChoferResponseDTO> choferesMostrados = choferService.obtenerAll();
-//        assertNotNull(choferesMostrados);
-//        assertEquals(choferesMostrados.size(),choferes.size());
-//        assertEquals(choferesMostrados.get(0).getIdChofer(),choferes.get(0).getIdChofer());
-//        assertEquals(choferesMostrados.get(1).getIdChofer(),choferes.get(1).getIdChofer());
-//    }
-//
-//    public void verificarNoRegistroDeChoferInvalido(){
-//        assertThrows(BadRequestException.class, () -> choferService.registro(choferRegistro));
-//        verify(choferRepository, never()).save(any(Chofer.class));
-//    }
-//
-//}
+package com.gifa_api.testUnitario.service;
+
+import com.gifa_api.dto.chofer.AsignarChoferDTO;
+import com.gifa_api.dto.chofer.ChoferRegistroDTO;
+import com.gifa_api.dto.chofer.ChoferResponseDTO;
+import com.gifa_api.exception.BadRequestException;
+import com.gifa_api.exception.NotFoundException;
+import com.gifa_api.model.Chofer;
+import com.gifa_api.model.Vehiculo;
+import com.gifa_api.repository.IChoferRepository;
+import com.gifa_api.repository.IVehiculoRepository;
+import com.gifa_api.service.impl.ChoferServiceImpl;
+import com.gifa_api.utils.mappers.ChoferMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+//agregar metodos que faltan
+@ExtendWith(MockitoExtension.class)
+class ChoferServiceImplTest {
+    @Mock
+    private IVehiculoRepository vehiculoRepository;
+
+    @Mock
+    private IChoferRepository choferRepository;
+
+    @Mock
+    private  PasswordEncoder passwordEncoder;
+    @Mock
+    private  ChoferMapper choferMapper;
+    @InjectMocks
+    private ChoferServiceImpl choferService;
+
+    private ChoferRegistroDTO choferRegistro;
+    private Chofer chofer;
+    private  AsignarChoferDTO asignarChoferDTO;
+    private Vehiculo vehiculo;
+    @BeforeEach
+    void setUp(){
+        choferRegistro = ChoferRegistroDTO.builder().username("username").password("password").nombre("nombre").build();
+        asignarChoferDTO = AsignarChoferDTO.builder().idChofer(1).idVehiculo(1).build();
+
+        vehiculo = Vehiculo.builder()
+                .chofers(Set.of(Chofer.builder().id(1).
+                        nombre("chofer")
+                        .vehiculo(vehiculo)
+                        .build()))
+                .id(1)
+                .patente("ABC123")
+                .build();
+
+        chofer = Chofer.builder().id(1).
+                nombre("chofer")
+                .vehiculo(vehiculo)
+                .build();
+
+    }
+
+    @Test
+    void registrarChofer_userNameVacioLanzaExcepcion(){
+        choferRegistro.setUsername("");
+        verificarNoRegistroDeChoferInvalido();
+    }
+
+    @Test
+    void campoUserNameNoPuedeSerNulo(){
+        choferRegistro.setUsername(null);
+        verificarNoRegistroDeChoferInvalido();
+    }
+
+    @Test
+    void campoUserNameNoPuedeContenerCaracteresEspeciales(){
+        choferRegistro.setUsername("fede#");
+        verificarNoRegistroDeChoferInvalido();
+    }
+
+    @Test
+    void campoContraseniaoNoPuedeSerNulo(){
+        choferRegistro.setPassword(null);
+        verificarNoRegistroDeChoferInvalido();
+    }
+
+    @Test
+    void campoContraseniaNoPuedeEstarVacio(){
+        choferRegistro.setPassword("");
+        verificarNoRegistroDeChoferInvalido();
+    }
+
+    @Test
+    void registrarChofer_nombreVacioLanzaExcepcion(){
+        choferRegistro.setNombre("");
+        verificarNoRegistroDeChoferInvalido();
+    }
+
+
+    @Test
+    void campoNombreNoPuedeSerNulo(){
+        choferRegistro.setNombre(null);
+        verificarNoRegistroDeChoferInvalido();
+    }
+
+    @Test
+    void registrarChofer_usuarioConCaracteresEspecialesLanzaExcepcion(){
+        choferRegistro.setUsername("diegote#");
+        verificarNoRegistroDeChoferInvalido();
+    }
+    @Test
+    void registrarChofer_contraseniaVaciaLanzaExcepcion(){
+        choferRegistro.setPassword("");
+        verificarNoRegistroDeChoferInvalido();
+    }
+
+    @Test
+    void inhabilitarUsuarioChoferConUsuarioInvalido_lanzaExcepcion(){
+        when(choferRepository.findByUsuario_Id(1)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> choferService.inhabilitarUsuarioChofer(1));
+
+        verify(choferRepository,never()).save(any(Chofer.class));
+
+    }
+    @Test
+    void inhabilitarUsuario_inhabilitaUsuarioExitosamente(){
+        when(choferRepository.findByUsuario_Id(1)).thenReturn(Optional.of(chofer));
+
+        choferService.inhabilitarUsuarioChofer(1);
+
+        assertEquals(0,vehiculo.getChofers().size());
+        assertEquals(chofer.getId(),1);
+        verify(choferRepository,times(1)).save(any(Chofer.class));
+
+    }
+
+    @Test
+
+    void asignarVehiculo_NoSeEncuentraVehiculoParaElChofer(){
+        when(vehiculoRepository.findById(asignarChoferDTO.getIdVehiculo())).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class,() -> choferService.asignarVehiculo(asignarChoferDTO));
+
+        verify(choferRepository,never()).save(any(Chofer.class));
+        verify(vehiculoRepository,times(1)).findById(asignarChoferDTO.getIdVehiculo());
+    }
+
+    @Test
+    void asignarVehiculo_NoSeEncuentraElChofer(){
+        when(vehiculoRepository.findById(asignarChoferDTO.getIdVehiculo())).thenReturn(Optional.of(new Vehiculo()));
+        when(choferRepository.findById(asignarChoferDTO.getIdChofer())).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class,() -> choferService.asignarVehiculo(asignarChoferDTO));
+
+        verify(vehiculoRepository,times(1)).findById(asignarChoferDTO.getIdVehiculo());
+        verify(choferRepository,times(1)).findById(asignarChoferDTO.getIdVehiculo());
+        verify(choferRepository,never()).save(any(Chofer.class));
+    }
+
+    @Test
+    void asignarVehiculo(){
+        Vehiculo vehiculo = new Vehiculo();
+        when(vehiculoRepository.findById(asignarChoferDTO.getIdVehiculo())).thenReturn(Optional.of(vehiculo));
+        when(choferRepository.findById(asignarChoferDTO.getIdChofer())).thenReturn(Optional.of(chofer));
+
+        choferService.asignarVehiculo(asignarChoferDTO);
+
+        verify(vehiculoRepository,times(1)).findById(asignarChoferDTO.getIdVehiculo());
+        verify(choferRepository,times(1)).findById(asignarChoferDTO.getIdChofer());
+        verify(choferRepository,times(1)).save(any(Chofer.class));
+        assertEquals(chofer.getVehiculo(),vehiculo);
+    }
+
+    @Test
+    void registrarChoferDebeGuardarChoferSiVehiculoExiste() {
+        ChoferRegistroDTO choferRegistroDTO = new ChoferRegistroDTO("chofer","1234","CHOFER");
+
+        when(passwordEncoder.encode(choferRegistroDTO.getPassword())).thenReturn("1234");
+        choferService.registro(choferRegistroDTO);
+
+        verify(choferRepository, times(1)).save(any(Chofer.class));
+    }
+
+
+
+    @Test
+    void obtenerTodosLosChoferes(){
+        ChoferResponseDTO chofer1 = ChoferResponseDTO.builder().nombre("chofer1").build();
+        ChoferResponseDTO chofer2 = ChoferResponseDTO.builder().nombre("chofer2").build();
+        List<ChoferResponseDTO> choferes = List.of(chofer1,chofer2);
+
+        when(choferMapper.obtenerListaChoferDTO(choferRepository.findAll())).thenReturn(choferes);
+
+        List<ChoferResponseDTO> choferesMostrados = choferService.obtenerAll();
+        assertNotNull(choferesMostrados);
+        assertEquals(choferesMostrados.size(),choferes.size());
+        assertEquals(choferesMostrados.get(0).getIdChofer(),choferes.get(0).getIdChofer());
+        assertEquals(choferesMostrados.get(1).getIdChofer(),choferes.get(1).getIdChofer());
+    }
+
+    public void verificarNoRegistroDeChoferInvalido(){
+        assertThrows(BadRequestException.class, () -> choferService.registro(choferRegistro));
+        verify(choferRepository, never()).save(any(Chofer.class));
+    }
+
+}
