f80d9b73a41f80a75495472d4438571746f56374
francoleon42 <francoleonadress@gmail.com>
2024-11-15 15:03:47 -0300

    delete interfaz de posicion service

--- src/main/java/com/gifa_api/controller/TraccarController.java
+++ src/main/java/com/gifa_api/controller/TraccarController.java
@@ -1,19 +1,13 @@
 package com.gifa_api.controller;
 
-import com.gifa_api.dto.traccar.CrearDispositivoRequestDTO;
-import com.gifa_api.dto.traccar.VerInconsistenciasRequestDTO;
 import com.gifa_api.service.IDispositivoService;
-import com.gifa_api.service.IPosicionService;
 import com.gifa_api.service.ITraccarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/gifa_api/service/impl/PosicionServiceImpl.java
+++ src/main/java/com/gifa_api/service/impl/PosicionServiceImpl.java
@@ -1,18 +1,13 @@
 package com.gifa_api.service.impl;
 
 import com.gifa_api.client.ITraccarCliente;
-import com.gifa_api.dto.traccar.DispositivoResponseDTO;
-import com.gifa_api.dto.traccar.PosicionRequestDTO;
 import com.gifa_api.dto.traccar.PosicionResponseDTO;
-import com.gifa_api.model.Dispositivo;
 import com.gifa_api.model.Posicion;
 import com.gifa_api.repository.IPosicionRepository;
 import com.gifa_api.service.IDispositivoService;
-import com.gifa_api.service.IPosicionService;
 import com.gifa_api.service.ITraccarService;
 import com.gifa_api.utils.mappers.PosicionMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -20,7 +15,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class PosicionServiceImpl implements IPosicionService {
+public class PosicionServiceImpl  {
     private final IPosicionRepository posicionRepository;
     private final ITraccarCliente traccarCliente;
     private final ITraccarService traccarService;
@@ -30,12 +25,12 @@ public class PosicionServiceImpl implements IPosicionService {
 
 
 
-    @Override
+
     public List<PosicionResponseDTO> getPosicionesDeDispositivo(String unicoID) {
         return posicionMapper.toPosicionResponseDTOList(posicionRepository.findByUnicoId(unicoID));
     }
 
-    @Override
+    
     public List<Posicion> getPosicionesDeDispositivoDespuesDeFecha(String unicoID, OffsetDateTime fecha) {
         return posicionRepository.findByUnicoIdAndDespuesFecha(unicoID, fecha);
     }
