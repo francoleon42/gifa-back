a37890b7554e749210f446536055c7a7bd26559e
francoleon42 <francoleonadress@gmail.com>
2024-11-15 15:09:27 -0300

    delete de toPosicionResponseDTOList en  posicion mapper

--- src/main/java/com/gifa_api/utils/mappers/PosicionMapper.java
+++ src/main/java/com/gifa_api/utils/mappers/PosicionMapper.java
@@ -10,20 +10,6 @@ import java.util.stream.Collectors;
 @Component
 public class PosicionMapper {
 
-    public PosicionResponseDTO toPosicionResponseDTO(Posicion posicion) {
-        return PosicionResponseDTO.builder()
-                .id(posicion.getId())
-                .latitude(posicion.getLatitude())
-                .longitude(posicion.getLongitude())
-                .fechaHora(posicion.getFechaHora().toLocalDate())
-                .build();
-    }
-
-    public List<PosicionResponseDTO> toPosicionResponseDTOList(List<Posicion> posiciones) {
-        return posiciones.stream()
-                .map(this::toPosicionResponseDTO)
-                .collect(Collectors.toList());
-    }
 
 
     public PosicionResponseDTO toPoscicionResponseFromPosicionRequest(PosicionRequestDTO posicionRequestDTO) {
