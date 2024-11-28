b61714696ad26b09726c1479b99c21d3d531d790
MicaBelle <84885589+MicaBelle@users.noreply.github.com>
2024-11-28 00:37:09 -0300

    Update SecurityConfig.java

--- src/main/java/com/gifa_api/config/SecurityConfig.java
+++ src/main/java/com/gifa_api/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://23.20.99.69"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://23.20.99.69"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
